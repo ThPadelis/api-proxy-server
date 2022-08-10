95eccdbce8d057f9aeb38c240e09a0bcb3735376
Pantelis Theodosiou <padelis.theodosiou@gmail.com>
2022-08-10 13:09:22 +0300

    Minor update
    - Add RateLimit package
        Functionality to limit requests per user

--- server/index.js
+++ server/index.js
@@ -2,14 +2,24 @@
 const express = require("express");
 const cors = require("cors");
 const env = require("./src/utils/env");
+const { rateLimit } = require("express-rate-limit");
 
 // Application
 const app = express();
+
+// Rate limit
+const createLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 5, // 5 requests per window
+  message:
+    "Too many accounts created from this IP, please try again after 5 minutes",
+});
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(createLimiter);
 
 // Routes
 app.use("/api", require("./src/routes")); // Every request that starts with /api will be handled by this handler
