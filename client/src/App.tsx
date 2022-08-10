d0be61ea222a81f733b4fc7009de0aed682dd966
Pantelis Theodosiou <padelis.theodosiou@gmail.com>
2022-08-10 17:45:44 +0300

    Addition of the UI
    - Add React for making the user interface of the app
        Basic integration and styling

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-all'>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react"
+import { useDarkMode } from "usehooks-ts"
+
+const Navbar = () => {
+    const { isDarkMode } = useDarkMode(true);
+
+    useEffect(() => {
+        const className = "dark"
+        const element = window.document.body;
+        isDarkMode ? element.classList.add(className) : element.classList.remove(className);
+    }, [isDarkMode]);
+
+    return <nav className="bg-all shadow-lg">
+        <div className="max-w-6xl mx-auto px-4">
+            <div className="flex items-center justify-between">
+                <div>
+                    <a href="#!" className="flex items-center py-4 px-2">
+                        <span className="font-semibold text-lg text-all">API Proxy Server</span>
+                    </a>
+                </div>
+                <div className="flex items-center">
+                    <Icon prefix="fa-brands" icon="fa-github" extraClass="text-lg text-all py-4 px-2" />
+                    <Icon icon={isDarkMode ? 'fa-moon' : 'fa-sun'} extraClass="text-lg text-all py-4 px-2" />
+                </div>
+            </div>
+        </div>
+    </nav>
+}
+
+const Icon = ({ icon, prefix = "fa-solid", extraClass }: IconProps) => {
+    const _extraClass = String(extraClass).split(" ").map(x => x.trim());
+    const className = [prefix, icon, ..._extraClass].join(" ");
+    const { toggle } = useDarkMode()
+    return (
+        <button onClick={toggle} className="">
+            <span className={className}></span>
+        </button>
+    )
+}
+
+interface IconProps {
+    prefix?: "fa-brands" | "fa-regular" | "fa-solid";
+    icon: string;
+    extraClass?: string
+}
+
+export default Navbar
